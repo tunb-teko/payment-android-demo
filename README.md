091ba9268e12591cc9a0e499767e126fa2b5021c
tunb-teko <tu.nb@teko.vn>
2021-04-30 17:02:45 +0700

    Get user info of VNPay E-Wallet

--- README.md
+++ README.md
@@ -17,12 +17,24 @@ Notes: Please contact Terra team (tu.nb@teko.vn) to get `userName` and `password
 
 ## VNPay E-Wallet
 
+Check out more details about VNPay EWallet at [sdkwallet](https://demomb.vnpay.vn:20157/sdk/sdkwallet/).
+
+**Account info:**
+
+- phone: `0999999998`
+- name: `Nguyen`
+- OTP: `123456`
+- PIN: `123456`
+
+Example: [MainActivity](app/src/main/java/vn/teko/payment/demo/MainActivity.kt)
+
 ### openVNPayEWallet
 
 ```kotlin
 class MainActivity: Activity() {
-    private fun openVNPayEWallet() {
+    private fun openVNPayEWalletDeprecated() {
         try {
+            // this method is deprecated, please use `VNPayEWalletUtils.openVNPayEWallet` instead
             paymentGateway.openVNPayEWallet(
                 context = this,
                 customer = VNPayEWalletCustomer(
@@ -35,7 +47,67 @@ class MainActivity: Activity() {
             Log.e("openVNPayEWallet error", "${error.message}")
         }
     }
+
+    private fun openVNPayEWallet() {
+        try {
+            VNPayEWalletUtils.openVNPayEWallet(
+                paymentGateway = paymentGateway,
+                context = this,
+                customer = VNPayEWalletCustomer(
+                    phone = "0999999998",
+                    name = "Nguyen",
+                    email = "" // optional
+                )
+            )
+        } catch (error: Throwable) {
+            Log.e("openVNPayEWallet error", "${error.message}")
+        }
+    }
 }
+```
+
+### getUserInfo
+
+```kotlin
+class MainActivity: Activity() {
+    private fun getUserInfo() {
+        VNPayEWalletUtils.getUserInfo(
+            paymentGateway = paymentGateway,
+            context = this,
+            phoneNumber = "0999999998"
+        ) { responseCode: Int, dataJson: String ->
+
+            /*
+            // Format data query
+            {
+                // mã code:
+                // 0 => Thành công
+                // -1 => User chưa liên kết với SDK Ví, các mã lỗi khác của sdk chỉ handle show message
+                // -2 => Số điện thoại, partner ID truyền vào rỗng hoặc NULL
+                // -3 => Có lỗi khi request API phía client ví (timeout, nullpointer, ...)
+                // -4 => Có lỗi khi request API phía server ví, có kèm theo mã lỗi  của server.
+                "code": 0,
+                "desc": "Success", // response mesage
+                "data": {
+                    "kyc_status": "1", // trạng thái kyc: 1 - kyc đã được duyệt; 0 - chưa kyc hoặc đã kyc nhưng chưa đc duyệt
+                    "balance": "20000", // số dư ví
+                    "bank_linked": 0 // trạng thái liên kết ngân hàng: 1 - đã lknh; 0 - chưa lknh
+                }
+            }
+             */
+
+            val userInfoResponse = Gson().fromJson(dataJson, UserInfoResponse::class.java)
+
+            AlertDialog.Builder(this)
+                .setMessage("Response code: $responseCode\nUser info: $userInfoResponse")
+                .setPositiveButton("OK") { _, _ -> }
+                .create()
+                .show()
+
+        }
+    }
+}
+
 ```
 
 ### payWithVNPayEWallet
--- app/src/main/java/vn/teko/payment/demo/MainActivity.kt
+++ app/src/main/java/vn/teko/payment/demo/MainActivity.kt
@@ -5,12 +5,13 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
+import okio.GzipSource
 import timber.log.Timber
 import vn.teko.android.payment.manager.TerraPayment
-import vn.teko.android.payment.ui.singlepayment.vnpayewallet.VNPayEWalletCallback
-import vn.teko.android.payment.ui.singlepayment.vnpayewallet.VNPayEWalletCustomer
-import vn.teko.android.payment.ui.singlepayment.vnpayewallet.VNPayEWalletPaymentRequest
-import vn.teko.android.payment.ui.singlepayment.vnpayewallet.VNPayEWalletSubscription
+import vn.teko.android.payment.ui.singlepayment.vnpayewallet.*
 import vn.teko.android.payment.ui.util.extension.openVNPayEWallet
 import vn.teko.android.payment.ui.util.extension.payWithVNPayEWallet
 import vn.teko.android.payment.v2.IPaymentGateway
@@ -40,6 +41,10 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.btnPayWithVNPayEWallet).setOnClickListener {
             payWithVNPayEWallet()
         }
+
+        findViewById<Button>(R.id.btnGetUserInfo).setOnClickListener {
+            getUserInfo()
+        }
     }
 
     override fun onDestroy() {
@@ -47,9 +52,25 @@ class MainActivity : AppCompatActivity() {
         vnpayEWalletSubscription?.cancel()
     }
 
-    private fun openVNPayEWallet() {
+    private fun openVNPayEWalletDeprecated() {
         try {
             paymentGateway.openVNPayEWallet(
+                context = this,
+                customer = VNPayEWalletCustomer(
+                    phone = "0999999998",
+                    name = "Nguyen",
+                    email = "" // optional
+                )
+            )
+        } catch (error: Throwable) {
+            Log.e("openVNPayEWallet error", "${error.message}")
+        }
+    }
+
+    private fun openVNPayEWallet() {
+        try {
+            VNPayEWalletUtils.openVNPayEWallet(
+                paymentGateway = paymentGateway,
                 context = this,
                 customer = VNPayEWalletCustomer(
                     phone = "0999999998",
@@ -89,4 +110,63 @@ class MainActivity : AppCompatActivity() {
             }
         )
     }
+
+    private fun getUserInfo() {
+        VNPayEWalletUtils.getUserInfo(
+            paymentGateway = paymentGateway,
+            context = this,
+            phoneNumber = "0999999998"
+        ) { responseCode: Int, dataJson: String ->
+
+            /*
+            // Format data query
+            {
+                // mã code:
+                // 0 => Thành công
+                // -1 => User chưa liên kết với SDK Ví, các mã lỗi khác của sdk chỉ handle show message
+                // -2 => Số điện thoại, partner ID truyền vào rỗng hoặc NULL
+                // -3 => Có lỗi khi request API phía client ví (timeout, nullpointer, ...)
+                // -4 => Có lỗi khi request API phía server ví, có kèm theo mã lỗi  của server.
+                "code": 0,
+                "desc": "Success", // response mesage
+                "data": {
+                    "kyc_status": "1", // trạng thái kyc: 1 - kyc đã được duyệt; 0 - chưa kyc hoặc đã kyc nhưng chưa đc duyệt
+                    "balance": "20000", // số dư ví
+                    "bank_linked": 0 // trạng thái liên kết ngân hàng: 1 - đã lknh; 0 - chưa lknh
+                }
+            }
+             */
+
+            val userInfoResponse = Gson().fromJson(dataJson, UserInfoResponse::class.java)
+
+            AlertDialog.Builder(this)
+                .setMessage("Response code: $responseCode\nUser info: $userInfoResponse")
+                .setPositiveButton("OK") { _, _ -> }
+                .create()
+                .show()
+
+        }
+    }
+}
+
+data class UserInfoResponse(
+    @SerializedName("code")
+    val code: String = "",
+
+    @SerializedName("desc")
+    val desc: String = "",
+
+    @SerializedName("data")
+    val data: Data? = null
+) {
+    data class Data(
+        @SerializedName("kyc_status")
+        val kycStatus: String = "",
+
+        @SerializedName("balance")
+        val balance: String = "",
+
+        @SerializedName("bank_linked")
+        val bankLinked: String = ""
+    )
 }
